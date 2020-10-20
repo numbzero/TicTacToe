e35e8cb755608257c470bcb9ee10c4c9625e135a
numbzero <numb0zero@gmail.com>
2020-10-21 01:31:03 +0300

    Minimax algorithm not implemented.

---
+++ script/script.js
@@ -0,0 +1,193 @@
+
+const	canvas 		= document.querySelector('canvas');
+const	ctx			= canvas.getContext('2d');
+const 	blockSize	= 100;
+var		matrix		= [[0, 0, 0],
+					   [0, 0, 0],
+					   [0, 0, 0]];
+var		whichTurn	= 'X';
+var		gameFinished = false;
+
+ctx.font = '48px roboto';
+ctx.fillStyle 	= `rgb(0, 0, 0)`;
+ctx.fillRect(0, 0, canvas.width /* width */, canvas.height /* height */);
+ctx.strokeStyle = `rgb(255, 0, 0)`;
+ctx.lineWidth 	= 10;
+ctx.strokeRect(0, 0, canvas.width /* width */, canvas.height /* height */);
+
+/* Draw # */
+ctx.lineWidth 	= 5;
+ctx.beginPath();
+ctx.moveTo(blockSize, 0);
+ctx.lineTo(blockSize, blockSize * 3);
+ctx.moveTo(blockSize * 2, 0);
+ctx.lineTo(blockSize * 2, blockSize * 3);
+ctx.moveTo(0, blockSize);
+ctx.lineTo(blockSize * 3, blockSize);
+ctx.moveTo(0, blockSize * 2);
+ctx.lineTo(blockSize * 3, blockSize * 2);
+ctx.stroke();
+
+canvas.addEventListener('click', function(event) {
+	let obj = {
+		x: event.offsetX,
+		y: event.offsetY,
+		cellX: event.offsetX / blockSize >> 0,
+		cellY: event.offsetY / blockSize >> 0
+	}
+
+	if (gameFinished != true) {
+
+		if ((matrix[obj.cellY][obj.cellX] === 0) ()) {
+			drawElement(whichTurn, obj.cellX, obj.cellY);
+			if (check()) {
+				console.log('Game finished!');
+				gameFinished = true;
+			}
+			whichTurn = (whichTurn === 'X') ? 'O' : 'X';
+		}
+
+	} else {
+		resetGame();
+	}
+});
+
+function	drawElement(element, x, y) {
+	if (element === 'X') {
+		drawX(x, y);
+	}
+	if (element === 'O') {
+		drawO(x, y);
+	}
+}
+
+function	drawX(x, y) {
+	matrix[y][x] = 'X';
+	ctx.beginPath();
+	ctx.moveTo(x * blockSize, y * blockSize);
+	ctx.lineTo(x * blockSize + blockSize,  y * blockSize + blockSize);
+	ctx.moveTo(x * blockSize + blockSize,  y * blockSize);
+	ctx.lineTo(x * blockSize, y * blockSize + blockSize);
+	ctx.stroke();
+
+}
+
+function	drawO(x, y) {
+	let 	circle;
+
+	matrix[y][x] = 'O';
+	circle = new Path2D();
+    circle.arc(x * blockSize + blockSize / 2, y * blockSize + blockSize / 2, blockSize / 3, 0, 2 * Math.PI);
+    ctx.stroke(circle);
+}
+
+function	check() {
+	if (checkLine()) {
+		showEndGame(whichTurn + ' wins!');
+		return true;
+	}
+	else if (checkCol()) {
+		showEndGame(whichTurn + ' wins!');
+		return true;
+	}
+	else if (checkDiag()) {
+		showEndGame(whichTurn + ' wins!');
+		return true;
+	}
+	else if (checkDraw()) {
+		showEndGame('Draw!');
+		return true;
+	}
+	return false;
+}
+
+function	checkLine() {
+	
+	for (let i = 0; i < 3; i++) {
+		if (matrix[i][0] === matrix[i][1] && matrix[i][1] === matrix[i][2] && matrix[i][0] != 0) {
+			console.log('Check line true');
+			return true;
+		}
+	}
+	return false;
+}
+
+function	checkCol() {
+	
+	for (let i = 0; i < 3; i++) {
+		if (matrix[0][i] === matrix[1][i] &&  matrix[1][i] ===  matrix[2][i] && matrix[0][i] != 0) {
+			console.log('Check column true');
+			return true;
+		}
+	}
+	return false;
+	
+}
+
+function	checkDiag() {
+	
+	if (matrix[1][1] != 0 && ((matrix[0][0] === matrix[1][1] && matrix[1][1] === matrix[2][2]) || (matrix[0][2] === matrix[1][1] && matrix[1][1] === matrix[2][0]))) {
+		console.log('Check diagonal true');
+		return true;
+	}
+	return false;
+}
+
+
+function	checkDraw() {
+	console.log('checkDraw');
+	for (let i = 0; i < 3; i++) {
+		for (let j = 0; j < 3; j++) {
+			if (matrix[i][j] === 0) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+
+function 	showEndGame(message) {
+	// ctx.fillStyle 	= `rgb(0, 0, 0)`;
+	// ctx.fillRect(0, 0, canvas.width /* width */, canvas.height /* height */);
+	// ctx.strokeStyle = `rgb(255, 0, 0)`;
+	ctx.lineWidth 	= 10;
+	// ctx.strokeRect(0, 0, canvas.width /* width */, canvas.height /* height */);	
+
+	/* Draw text */
+	ctx.fillStyle 	= `rgb(0, 255, 0)`;
+	ctx.fillText(message, blockSize, blockSize * 3 / 2);
+}
+
+function 	resetGame() {
+	/* Reset values */
+	whichTurn	 = 'X';
+	gameFinished = false;
+
+	for (let i = 0; i < 3; i++) {
+		for (let j = 0; j < 3; j++) {
+			matrix[i][j] = 0;
+		}
+	}
+
+	/* Redraw # */
+	drawBoard();
+}
+
+function	drawBoard() {
+	ctx.fillStyle 	= `rgb(0, 0, 0)`;
+	ctx.fillRect(0, 0, canvas.width /* width */, canvas.height /* height */);
+	ctx.strokeStyle = `rgb(255, 0, 0)`;
+	ctx.lineWidth 	= 5;
+	ctx.strokeRect(0, 0, canvas.width /* width */, canvas.height /* height */);
+	ctx.beginPath();
+	ctx.moveTo(blockSize, 0);
+	ctx.lineTo(blockSize, blockSize * 3);
+	ctx.moveTo(blockSize * 2, 0);
+	ctx.lineTo(blockSize * 2, blockSize * 3);
+	ctx.moveTo(0, blockSize);
+	ctx.lineTo(blockSize * 3, blockSize);
+	ctx.moveTo(0, blockSize * 2);
+	ctx.lineTo(blockSize * 3, blockSize * 2);
+	ctx.stroke();	
+}
